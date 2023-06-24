b9ac0334a5280fe3e260fdfa3028c5943f993b6c
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-24 12:18:34 +0200

    Job controllers, and routes done

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const jobRouter = express.Router();
 
-const { findById, add, remove, edit, findByOwnerId, findByStatus, findByTenantId, getAll } = require('../controllers/job.controllers');
+const { findById, add, remove, edit, getAll } = require('../controllers/job.controllers');
 
 jobRouter.post('/add', add);
 jobRouter.get('/list', getAll);
 jobRouter.get('/findById', findById);
-jobRouter.get('/findByOwnerId', findByOwnerId);
-jobRouter.get('/findByStatus', findByStatus);
-jobRouter.get('/findByTenantId', findByTenantId);
 jobRouter.put('/update', edit);
 jobRouter.delete('/delete', remove);
 
